b8e7a2a90dae779f01d26280ea6d29b2aec6f83c
Sushil Tiwari <sushiltiwari@XIPL91232.local>
2021-06-30 19:01:30 +0530

    msg

---
+++ Example/Pods/Target Support Files/Pods-printo_Example/Pods-printo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_printo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_printo_Example
+@end
---
+++ Example/Pods/Target Support Files/printo/printo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_printo : NSObject
+@end
+@implementation PodsDummy_printo
+@end
